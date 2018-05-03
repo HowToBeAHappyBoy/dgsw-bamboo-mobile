4c69f8dc50bc4b4e146e9c2217a74bbb4a3c58d8
Kimjio <kimjioh0927@gmail.com>
2018-05-03 19:05:31 +0900

    String, UI 수정 및 compileSdk, buildTool, dependencies 버전 수정

--- app/src/main/java/com/dgsw/bamboo/data/Data.java
+++ app/src/main/java/com/dgsw/bamboo/data/Data.java
@@ -3,7 +3,7 @@ package com.dgsw.bamboo.data;
 public class Data {
     public static String adminName;
     public static String token;
-
+    //TODO Encryption
     public static boolean isEmpty() {
         return adminName == null && token == null;
     }
